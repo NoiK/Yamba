aadffdad1a1a84f81c52e8177e046c81eb006e73
Noik00 <noik00@gmail.com>
2014-06-10 22:37:40 +0200

    added a StatusProvider

--- Yamba/src/main/java/nl/noik/yamba/RefreshService.java
+++ Yamba/src/main/java/nl/noik/yamba/RefreshService.java
@@ -6,9 +6,7 @@ import android.app.IntentService;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
-//import android.net.Uri;
-import android.database.sqlite.SQLiteDatabaseLockedException;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -47,13 +45,14 @@ public class RefreshService extends IntentService {
 
         Log.d(TAG, "onStarted");
 
-        DbHelper dbHelper = new DbHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        //DbHelper dbHelper = new DbHelper(this);
+        //SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
 
         YambaClient cloud = new YambaClient(username, password);
         try {
-            List<Status> timeline = cloud.getTimeline(2);
+            int count = 0;
+            List<Status> timeline = cloud.getTimeline(20);
             for (Status status : timeline) {
                 values.clear();
                 values.put(StatusContract.Column.ID, status.getId());
@@ -61,7 +60,14 @@ public class RefreshService extends IntentService {
                 values.put(StatusContract.Column.MESSAGE, status.getMessage());
                 values.put(StatusContract.Column.CREATED_AT, status.getCreatedAt().getTime());
 
-                db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+                //db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+                Uri uri = getContentResolver().insert(StatusContract.CONTENT_URI, values);
+                if (uri != null) {
+                    count++;
+                    Log.d(TAG,
+                            String.format("%s: %s", status.getUser(), status.getMessage()));
+                }
 
                 Log.d(TAG,
                         String.format("%s: %s: %s", status.getUser(), status.getCreatedAt(),
--- Yamba/src/main/java/nl/noik/yamba/StatusContract.java
+++ Yamba/src/main/java/nl/noik/yamba/StatusContract.java
@@ -12,6 +12,14 @@ public class StatusContract {
     public static final int DB_VERSION = 1;
     public static final String TABLE = "status";
 
+    //Provider specific constants
+    // content://nl.noik.yamba.StatusProvider/status
+    public static final String AUTHORITY = "nl.noik.yamba.StatusProvider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE);
+    public static final int STATUS_ITEM = 1;
+    public static final int STATUS_DIR = 2;
+    public static final String STATUS_TYPE_ITEM = "vnd.android.cursor.item/vnd.nl.noik.yamba.provider.status";
+    public static final String STATUS_TYPE_DIR = "vnd.android.cursor.dir/vnd.nl.noik.yamba.provider.status";
     public static final String DEFAULT_SORT = Column.CREATED_AT + " DESC";
 
     public class Column {
@@ -20,5 +28,4 @@ public class StatusContract {
         public static final String MESSAGE = "message";
         public static final String CREATED_AT = "created_at";
     }
-
 }
