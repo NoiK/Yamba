8217f8149e62219452c7ad8ed47013e596605a67
Noik00 <noik00@gmail.com>
2014-06-13 00:30:36 +0200

    added ListFragment

--- Yamba/src/main/java/nl/noik/yamba/MainActivity.java
+++ Yamba/src/main/java/nl/noik/yamba/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//import android.widget.Toast;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -33,6 +33,12 @@ public class MainActivity extends Activity {
                 return true;
             case R.id.action_refresh:
                 startService(new Intent(this, RefreshService.class));
+                return true;
+            case R.id.action_purge:
+                int rows = getContentResolver().delete(
+                        StatusContract.CONTENT_URI, null, null);
+                Toast.makeText(this, "Deleted "+ rows + " rows" ,
+                        Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return false;
