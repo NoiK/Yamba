d34d5370a805918057e23eaae26b4096591750b1
Noik00 <noik00@gmail.com>
2014-06-15 21:58:38 +0200

    Added bootreceiver with alarm manager

--- Yamba/src/main/java/nl/noik/yamba/BootReceiver.java
+++ Yamba/src/main/java/nl/noik/yamba/BootReceiver.java
@@ -1,14 +1,42 @@
 package nl.noik.yamba;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = BootReceiver.class.getSimpleName();
+    private static final long DEFAULT_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, RefreshService.class));
-        Log.d("BootReceiver", "onReceive");
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        long interval = Long.parseLong(prefs.getString("interval", Long.toString(DEFAULT_INTERVAL)));
+
+        PendingIntent operation = PendingIntent.getService(context, -1,
+                new Intent(context, RefreshService.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+
+        if (interval == 0) {
+            alarmManager.cancel(operation);
+            Log.d(TAG, "Cancelling repeat operation");
+        } else {
+            alarmManager.setInexactRepeating(AlarmManager.RTC,
+                    System.currentTimeMillis(),
+                    interval,
+                    operation);
+            Log.d(TAG, "setting repeat operation for: " + interval);
+        }
+
+        //context.startService(new Intent(context, RefreshService.class));
+        Log.d(TAG, "onReceive");
     }
 }
